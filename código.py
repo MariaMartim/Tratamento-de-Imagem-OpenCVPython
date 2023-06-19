466e41ce969c60684a76c9c48d1c0114a2498c88
Maria Eduarda <126433251+DudaMartim@users.noreply.github.com>
2023-06-19 10:10:38 -0300

    Update código.py

--- código.py
+++ código.py
@@ -4,15 +4,15 @@ import numpy as np #redução da escrita numpy para np
 #caso for usar o Google Colab com a OpenCV, usar a lib abaixo
 from google.colab.patches import cv2_imshow #utilização da função cv2_imshow do google colab
 
-img = cv2.imread('j.png',0) #função da leitura da imagem
-img_opening = cv2.imread('j_ruido.png',0) #retirar pequenos ruidos da imagem, passagem da erosão pra dilatação
-img_closing = cv2.imread('j_furos.png',0) #dilata a imagem para que os furos preencham o j, e erode para voltar ao j sem furos
+img = cv2.imread('j.png',0) #armazenamento da imagem original à variavel "img"
+img_opening = cv2.imread('j_ruido.png',0) #armazena a imagem com ruidos na variavel "img_opening"
+img_closing = cv2.imread('j_furos.png',0) #armazena a imagem com furos na variavel "img_closing"
 altura, largura = img.shape #dimensões da imagem usando ".shape" e armazenamento nas variáveis
 kernel = np.ones((5,5),np.uint8) #matriz do kernel 5x5 preenchido por 1 e indicação de valores inteiros sem sinal de 8 bits
 print(kernel) #apresenta o kernel para visualização
 
-erosion = cv2.erode(img,kernel,iterations = 2) #aplica a erosão usando o kernel e o "iterations" corresponde a quantidade de vezes que a erosão é utilizada. resultado armazenado na variavel "erosion"
-dilation = cv2.dilate(img,kernel,iterations = 2) #mesma aplicação, apenas usando a função "dilation"
+erosion = cv2.erode(img,kernel,iterations = 2) #aplica a erosão na imagem original usando o kernel e o "iterations" corresponde a quantidade de vezes que a erosão é utilizada. resultado armazenado na variavel "erosion"
+dilation = cv2.dilate(img,kernel,iterations = 2) #mesma aplicação na imagem original, apenas usando a função "dilation"
 
 gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel) #a função gradiente destaca os contornos da imagem, subtração da imagem dilatada com a erodida. nesta função ele aplica utilizando a variavel kernel e armazena na variavel "gradient"
 opening = cv2.morphologyEx(img_opening, cv2.MORPH_OPEN, kernel) #função de abertura da erosão pra dilatação utilizando o kernel, armazenando na variavel "opening"
